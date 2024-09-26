2967a579dded22e52aa7d92a2f203e80d50a06aa
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-26 10:46:38 +0600

    default parameters value

--- peac01.js
+++ peac01.js
@@ -1,1 +1,9 @@
-console.log("first")
+//default parameters value
+
+function add(n1=0,n2=0){
+    const result = n1+n2;
+    console.log(n1, n2, result)
+    return result;
+}
+
+console.log(add(1,2))
