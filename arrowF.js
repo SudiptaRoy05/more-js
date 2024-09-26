532a0d3ee754dc5cb6948a50d5e1942155a4aecd
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-26 11:38:49 +0600

    arrow function

---
+++ arrowF.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(1,3));
+
+// arrow function 
+const add1 =(a,b)=> a+b;
+console.log(add1(3,5))
+
+const c = add(2,3);
+const d = add1(4,5)
+
+const add2 =(a,b) => a*b
+console.log(c,d);
