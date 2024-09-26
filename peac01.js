e7da93eb89ac25e60278d4949295ab2012ed2474
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-26 10:54:48 +0600

    default param value of string

--- peac01.js
+++ peac01.js
@@ -1,4 +1,9 @@
 //default parameters value
+// for number 
+// n1 = 0 
+// n2 = 0
+
+// default value can be any number as per the uses but if parameter takes string then ti will be empty sting
 
 function add(n1=0,n2=0){
     const result = n1+n2;
@@ -7,3 +12,16 @@ function add(n1=0,n2=0){
 }
 
 console.log(add(1,2))
+
+
+
+// default parameters value for string is empty string 
+// fName =" "
+// lName =" "
+function name(fName=" ", lName=" "){
+    const fullName = `${fName} ${lName}`;
+    console.log(fName, lName, fullName);
+    return fullName;
+}
+
+console.log(name('a','d'))
